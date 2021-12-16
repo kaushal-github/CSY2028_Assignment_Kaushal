a45156f23c42aafbf817d9120d1486ed12167c3b
kaushal ghimire <60461681+kaushal-github@users.noreply.github.com>
2021-12-16 18:11:15 +0545

    Update dbconnection.php

--- CSY2028_Assignment_Kaushal/dbconnection.php
+++ CSY2028_Assignment_Kaushal/dbconnection.php
@@ -6,4 +6,10 @@ $db_password = '';
 // $schema ='csy2028database';
 
 $pdo = new PDO('mysql:dbname='.$schema.';host='.$server , $db_username, $db_password, [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]) ;
+
+// Check connection
+// if ($pdo->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
 ?>
